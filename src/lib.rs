4fda6018363b466145920d86e0d9b8a5dfd1eed0
Pierre Jeanjacquot <26487010+PierreJeanjacquot@users.noreply.github.com>
2026-06-15 14:20:14 +0200

    refactor: expose internal lib to enable running integration tests from external crate

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+//! internal library
+
+pub mod application;
+pub mod config;
+pub mod crypto;
+pub mod error;
+pub mod handlers;
+pub mod kms;
+pub mod repository;
+pub mod rpc;
+pub mod types;
+pub mod validation;
